a9a3171b6ba977a649f247cac3d0ea11a143f40a
ZizyP <kaibi.0yassine@gmail.com>
2025-10-15 23:49:02 +0100

    removed user name field

--- backend/src/main/kotlin/com/pistoncontrol/routes/Models.kt
+++ backend/src/main/kotlin/com/pistoncontrol/routes/Models.kt
@@ -11,8 +11,7 @@ data class LoginRequest(
 @Serializable
 data class RegisterRequest(
     val email: String,
-    val password: String,
-    val name: String
+    val password: String
 )
 
 @Serializable
